43636afc5b92e385767c50152a0d7fb72b8f76ae
OLESIA CHUGAIEVA <ALESYAPRITCHARD@GMAIL.COM>
2022-09-21 14:45:41 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 const apiRoutes = require('./routes/apiroutes');
 const htmlRoutes = require('./routes/htmlroutes');
 
-// Creating an express server
+// Creating an Express server
 const app = express();
 
 // Setting a PORT
 const PORT = process.env.PORT || 3001;
 
-
+//Setting up data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//Routes
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
